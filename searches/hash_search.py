2e66f726de29b4fe8980280dc333201f1c2c356f
tomoyano <epicpoppop@gmail.com>
2021-11-01 22:29:01 +0900

    change hash search

--- searches/hash_search.py
+++ searches/hash_search.py
@@ -1,5 +1,6 @@
 """ハッシュ探索法
 """
+from pprint import pprint
 from typing import List
 
 
@@ -8,12 +9,22 @@ def create_keeper(length: int) -> List[int]:
 
 def add_to_list(keeper: List[int], target:int):
     # ハッシュ値
-    hash_value: int = target % length
+    hash_value: int = target % len(keeper)
 
     index: int = hash_value
-    while True:
+    for _ in range(len(keeper)):
         if keeper[index] == 0:
-            keeper[index] == target
+            keeper[index] = target
             break
         
         index = (index + 1) % len(keeper)
+
+
+if __name__ == '__main__':
+    keeper: List[int] = create_keeper(5)
+    add_to_list(keeper, 7)
+    add_to_list(keeper, 12)
+    add_to_list(keeper, 17)
+    add_to_list(keeper, 5)
+    add_to_list(keeper, 22)
+    pprint(keeper)
